8821f6d21f3c10f030b782b53543e7988dc685c3
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-07 08:25:29 +0700

    mengimport biodata.js, biodata2.js, project1.js, dan project2.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Biodata from './components/Biodata'
+import Biodata2 from './components/Biodata2'
+import Project1 from './components/Project1'
+import Project2 from './components/Project2'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Biodata />
+      <Biodata2 />
+      <Project1 />
+      <Project2 />
+    </div>
   )
 }
 
