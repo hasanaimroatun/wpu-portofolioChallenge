9d7cd07bc40ee3ac3657bf09e73b99ea7298acbb
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-07 20:00:14 +0700

    mengimport blog.js, blog2.js, skill1.js, dan skill2.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Biodata from './components/Biodata'
 import Biodata2 from './components/Biodata2'
 import Project1 from './components/Project1'
 import Project2 from './components/Project2'
+import Skill1 from './components/Skill1'
+import Skill2 from './components/Skill2'
+import Blog from './components/Blog'
+import Blog2 from './components/Blog2'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Biodata2 />
       <Project1 />
       <Project2 />
+      <Skill1 />
+      <Skill2 />
+      <Blog />
+      <Blog2 />
     </div>
   )
 }
