44c8063c906f3c4d78595aa965ce19eb30ccd2e4
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-10 10:02:07 +0700

    menghapus semua import kecuali portofolio1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import React from 'react'
-import Biodata from './components/Biodata'
-import Biodata2 from './components/Biodata2'
-import Project1 from './components/Project1'
-import Project2 from './components/Project2'
-import Skill1 from './components/Skill1'
-import Skill2 from './components/Skill2'
-import Blog from './components/Blog'
-import Blog2 from './components/Blog2'
-import Pengalaman1 from './components/Pengalaman1'
-import Pengalaman2 from './components/Pengalaman2'
-import Hobi1 from './components/Hobi1'
-import Hobi2 from './components/Hobi2'
-import ButtonUtama from './components/ButtonUtama'
-import ButtonUtama2 from './components/ButtonUtama2'
 import Portofolio1 from './components/Portofolio1'
 
 function App() {
   return (
     <div>
-      <Biodata />
-      <Biodata2 />
-      <Project1 />
-      <Project2 />
-      <Skill1 />
-      <Skill2 />
-      <Blog />
-      <Blog2 />
-      <Pengalaman1 />
-      <Pengalaman2 />
-      <Hobi1 />
-      <Hobi2 />
-      <ButtonUtama />
-      <ButtonUtama2 />
       <Portofolio1 />
     </div>
   )
