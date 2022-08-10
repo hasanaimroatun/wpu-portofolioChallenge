642e45c62b0a2c177ea580cfcd73dbfa3ccdddcf
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-10 10:04:04 +0700

    menambahkan class col dan row serta gap

--- src/components/Portofolio1.js
+++ src/components/Portofolio1.js
@@ -8,28 +8,32 @@ import ButtonUtama from './ButtonUtama'
 
 function Portofolio1() {
   return (
-    <div>
-        <div class="row g-0">
-            <Biodata />
-        </div>
-        <div class="row g-0">
-            <div class="col-md-3">
-                <div class="row g-0">
-                    <Skill2 />
-                </div>
-                <div class="row g-0">
-                    <Hobi2 />
+    <div class="row g-0">
+        <div class="col-md-10 offset-1">
+            <div class="row g-0">
+                <div class="col-md-11 m-2">
+                    <Biodata />
                 </div>
             </div>
-            <div class="col-md-8">
-                <div class="row g-0">
-                    <Pengalaman1 />
+            <div class="row g-0" style={{columnGap:"0.5rem"}}>
+                <div class="col-md-3">
+                    <div class="row g-0">
+                        <Skill2 />
+                    </div>
+                    <div class="row g-0">
+                        <Hobi2 />
+                    </div>
                 </div>
-                <div class="row g-0">
-                    <ButtonUtama2 />
-                </div>
-                <div class="row g-0">
-                    <ButtonUtama />
+                <div class="col-md-8">
+                    <div class="row g-0">
+                        <Pengalaman1 />
+                    </div>
+                    <div class="row g-0">
+                        <ButtonUtama2 />
+                    </div>
+                    <div class="row g-0">
+                        <ButtonUtama />
+                    </div>
                 </div>
             </div>
         </div>
