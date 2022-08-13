f74a03cc6c8b9311ae8f8f43946171082e15e2b7
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-13 19:19:43 +0700

    menambahkan link

--- src/components/CreatedBy.js
+++ src/components/CreatedBy.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CreatedBy() {
   return (
     <div class="container-fluid mt-5 text-center text-muted">
-        created by <a href="https://github.com/hasanaimroatun/wpu-randomQuotes" style={{color:"inherit"}}>hasanaimroatun</a> - devChallenges.io
+        created by <a href="https://github.com/hasanaimroatun/wpu-portofolioChallenge" style={{color:"inherit"}}>hasanaimroatun</a> - devChallenges.io
     </div>
   )
 }
