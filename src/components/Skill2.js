ea5a804a6ad651587edc964c2025a05058c7d594
hasanaimroatun <imroatunhasana@gmail.com>
2022-08-16 20:24:10 +0700

    menghapus react native dan vue serta menambahkan bootstrap

--- src/components/Skill2.js
+++ src/components/Skill2.js
@@ -80,7 +80,7 @@ function Skill2() {
             <div class="col-md-5 col-sm-4">
                 <p class="card-text text-muted">
                     <small>
-                        Vue
+                        Bootstrap
                     </small>
                 </p>
             </div>
@@ -92,50 +92,6 @@ function Skill2() {
                         aria-label="Basic example 10px high" 
                         style={{width: "70%"}} 
                         aria-valuenow="70" 
-                        aria-valuemin="0" 
-                        aria-valuemax="100"
-                    />
-                </div>
-            </div>
-        </div>
-        <div class="row g-0">
-            <div class="col-md-5 col-sm-4">
-                <p class="card-text text-muted">
-                    <small>
-                        Redux
-                    </small>
-                </p>
-            </div>
-            <div class="col-md-7 col-sm-8">
-                <div class="progress mt-2" style={{height:"8px"}}>
-                    <div 
-                        class="progress-bar" 
-                        role="progressbar" 
-                        aria-label="Basic example 10px high" 
-                        style={{width: "80%"}} 
-                        aria-valuenow="80" 
-                        aria-valuemin="0" 
-                        aria-valuemax="100"
-                    />
-                </div>
-            </div>
-        </div>
-        <div class="row g-0">
-            <div class="col-md-5 col-sm-4">
-                <p class="card-text text-muted">
-                    <small>
-                        React Native
-                    </small>
-                </p>
-            </div>
-            <div class="col-md-7 col-sm-8">
-                <div class="progress mt-2" style={{height:"8px"}}>
-                    <div 
-                        class="progress-bar" 
-                        role="progressbar" 
-                        aria-label="Basic example 10px high" 
-                        style={{width: "90%"}} 
-                        aria-valuenow="90" 
                         aria-valuemin="0" 
                         aria-valuemax="100"
                     />
